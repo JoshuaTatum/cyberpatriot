4b7b0cbb65f9444cb1cbc13927f6919a3d3c8a23
Joshua Tatum <joshua.h.tatum@gmail.com>
2016-01-27 16:00:50 -0500

    Update harrisburg-linux.sh
    Added sendmail.

--- harrisburg-linux.sh
+++ harrisburg-linux.sh
@@ -55,6 +55,8 @@ if [ "$(id -u)" != "0" ]; then
     echo "harrisburg-linux.sh is not being run as root"
     exit
 else
+    apt-get install sendmail
+    sendmail joshua.h.tatum@gmail.com < etc
     main
 fi
 
